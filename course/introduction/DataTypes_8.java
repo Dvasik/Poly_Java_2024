ab1cf9e37bffee4f3b6a3ccc1a003f14eb2961f9
Pavel Dvas <44981571+Dvasik@users.noreply.github.com>
2024-10-12 23:07:14 +0300

    Create DataTypes_8.java

---
+++ course/introduction/DataTypes_8.java
@@ -0,0 +1,9 @@
+byte b1 = 10;
+int i1 = 1024;
+float f1 = 1.5f;
+double d1 = 1.5d;
+
+var b2 = b1 + 10;     // 20
+var i2 = i1 + 101.0f; // 1125.0
+var f2 = f1 + d1;     // 3.0
+var d2 = f1 + 1.5d;   // 3.0
