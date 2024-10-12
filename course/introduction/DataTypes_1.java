0b700886757d1c8b361c46fe67cbf8293277f25a
Pavel Dvas <44981571+Dvasik@users.noreply.github.com>
2024-10-12 22:45:00 +0300

    Create DataTypes_1.java

---
+++ course/introduction/DataTypes_1.java
@@ -0,0 +1,23 @@
+byte b0 = 0;
+byte b1 = 10;
+
+short s0 = 0;
+short s1 = 128;
+
+int i0 = 0;
+int i1 = 1024;
+
+long l0 = 0;
+long l1 = 1235797785;
+
+float f0 = 0.0f;
+float f1 = 1.5f;
+
+double d0 = 0.0;
+double d1 = 1.5d;
+
+char c0 = 0;
+char c1 = 'a';
+
+boolean bl0 = false;
+boolean bl1 = true;
