17fd06b37a73e7762b3a664753f450cce06f5de2
Pavel Dvas <44981571+Dvasik@users.noreply.github.com>
2024-10-12 22:52:05 +0300

    Update DataTypes_1.java

--- course/introduction/DataTypes_1.java
+++ course/introduction/DataTypes_1.java
@@ -1,23 +1,27 @@
-byte b0 = 0;
-byte b1 = 10;
+public class Test {
+    public static void main(String args[]) {
+        byte b0 = 0;
+        byte b1 = 10;
 
-short s0 = 0;
-short s1 = 128;
+        short s0 = 0;
+        short s1 = 128;
 
-int i0 = 0;
-int i1 = 1024;
+        int i0 = 0;
+        int i1 = 1024;
 
-long l0 = 0;
-long l1 = 1235797785;
+        long l0 = 0;
+        long l1 = 1235797785;
 
-float f0 = 0.0f;
-float f1 = 1.5f;
+        float f0 = 0.0f;
+        float f1 = 1.5f;
 
-double d0 = 0.0;
-double d1 = 1.5d;
+        double d0 = 0.0;
+        double d1 = 1.5d;
 
-char c0 = 0;
-char c1 = 'a';
+        char c0 = 0;
+        char c1 = 'a';
 
-boolean bl0 = false;
-boolean bl1 = true;
+        boolean bl0 = false;
+        boolean bl1 = true;
+    }
+}
