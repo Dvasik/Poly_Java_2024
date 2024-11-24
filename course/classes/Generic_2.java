c3fca549ff5cabfa5faa2f464699d67048429364
Pavel Dvas <44981571+Dvasik@users.noreply.github.com>
2024-11-24 12:27:48 +0300

    Update Generic_2.java

--- course/classes/Generic_2.java
+++ course/classes/Generic_2.java
@@ -5,35 +5,29 @@ class Parent { }
 class A extends Parent { }
 class B extends Parent { }
 
+class SubA extends A { }
+
 // Класс с параметризированным типом
 class Wrapper<T> {
-    public List<T> list = new ArrayList<>();
-
-    // Метод, который принимает тип с ограничением <? super T>
-    public void printParent(List<? super T> parent) {
-    }
-
-    // Метод, который принимает тип с ограничением <? extends T>
-    public void printChild(List<? extends T> child) {
-    }
+    // Лист, в который можно помещать объекты типов T и его суперклассов
+    public List<? super T> listParent = new ArrayList<>();
+    // Лист, в который можно помещать объекты типов T и его подклассов
+    public List<? extends T> listChild;
 }
 
 public class Main {
     public static void main(String[] args) {
-        List<A> listA = new ArrayList<>();
-        List<B> listB = new ArrayList<>();
-        List<Parent> listParent = new ArrayList<>();
+        A a = new A();
+        B b = new B();
+        Parent p = new Parent();
+        SubA sa = new SubA();
 
         Wrapper<A> a1 = new Wrapper<>();
 
-        a1.printParent(listB); // Ошибка компиляции
-        a1.printParent(listA); // Выполняется корректно
-        a1.printParent(listParent); // Выполняется корректно
-        
-        Wrapper<Parent> p1 = new Wrapper<>();
-
-        p1.printParent(listA); // Ошибка компиляции
-        p1.printParent(listParent); // Выполняется корректно
-        p1.printChild(listA); // Выполняется корректно
+        a1.listChild = new ArrayList<Parent>(); // Ошибка компиляции
+        a1.listChild = new ArrayList<A>(); // Выполняется корректно
+        a1.listParent = new ArrayList<A>(); // Выполняется корректно
+        a1.listParent = new ArrayList<Parent>(); // Выполняется корректно
+        a1.listParent = new ArrayList<B>(); // Ошибка компиляции
     }
 }
