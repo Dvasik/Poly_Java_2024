dbc22ac53534469f1f156697e6a0f7b5193e49af
Pavel Dvas <44981571+Dvasik@users.noreply.github.com>
2024-11-30 20:35:11 +0300

    Create Excep3.java

---
+++ course/classes/Excep3.java
@@ -0,0 +1,27 @@
+public class ExceptionHandlingExample {
+    public static void main(String[] args) {
+        // Обработка нескольких исключений идентичным образом
+        try {
+            // ArithmeticException
+            int result = 10 / 0;
+
+            // NullPointerException
+            String str = null;
+            int length = str.length();
+        } catch (ArithmeticException | NullPointerException e) {
+            System.out.println("Exceptions");
+        }
+
+        // Иерархическая обработка исключений
+        try {
+            int[] numbers = {1, 2, 3};
+            System.out.println(numbers[5]); // ArrayIndexOutOfBoundsException
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("ArrayIndexOutOfBoundsException");
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("суперкласс IndexOutOfBoundsException");
+        } catch (RuntimeException e) {
+            System.out.println("суперкласс RuntimeException");
+        }
+    }
+}
