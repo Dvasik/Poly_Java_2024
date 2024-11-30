28d38a2f406a1e4976c1811a31e7976f2f3379d3
Pavel Dvas <44981571+Dvasik@users.noreply.github.com>
2024-11-30 21:06:08 +0300

    Create Inner_1.java

---
+++ course/classes/Inner_1.java
@@ -0,0 +1,18 @@
+// Внешний класс
+public class OuterClass {
+    // Внутренний публичный класс
+    public class PublicInner {
+    }
+  
+    // Внутренний защищенный класс
+    protected class ProtectedInner {   
+    }
+  
+    // Внутренний класс с доступом по умолчанию
+    class DefaultInner {    
+    }
+
+    // Внутренний приватный класс
+    private class PrivateInner {
+    }
+}
